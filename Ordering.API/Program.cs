c8be896b4d8c9378df29e05abaa605add9c6e347
Mateusz <mateusz.wolnica@gmail.com>
2024-07-28 13:59:20 +0200

    .

--- Ordering.API/Program.cs
+++ Ordering.API/Program.cs
@@ -1,4 +1,7 @@
 
+using BuildingBlocks.Behaviors;
+using Carter;
+
 namespace Ordering.API
 {
     public class Program
@@ -10,6 +13,16 @@ namespace Ordering.API
             // Add services to the container.
             // AddSecondWebApiClient
             // AddRabbitMqClient for choreography
+
+            // WIP
+            var assembly = typeof(Program).Assembly;
+            builder.Services.AddCarter();
+            builder.Services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(assembly);
+                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- Ordering.Infrastructure/obj/Debug/net8.0/Ordering.Infrastructure.AssemblyInfo.cs
+++ Ordering.Infrastructure/obj/Debug/net8.0/Ordering.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ordering.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a1a0304118e8baede10a89941c947ca94926d8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e67bcfcc38200504cea0cb43575f7ff1803acfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ordering.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ordering.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
