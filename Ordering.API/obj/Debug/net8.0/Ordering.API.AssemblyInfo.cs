cc7664d19ff5f0b5987f53bdef162dda8175f74e
Mateusz <mateusz.wolnica@gmail.com>
2024-07-29 23:29:50 +0200

    .

--- Ordering.API/obj/Debug/net8.0/Ordering.API.AssemblyInfo.cs
+++ Ordering.API/obj/Debug/net8.0/Ordering.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ordering.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+629f9b89308b785679c71de300f3b4bf86493119")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f495f1b3da2a3e7322804f10f514da1c8a79ec63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ordering.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ordering.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
