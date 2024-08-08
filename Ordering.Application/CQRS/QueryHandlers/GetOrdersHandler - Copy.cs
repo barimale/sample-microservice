8feafa2ac540f343d409ed78de025f7516c808c7
Mateusz <mateusz.wolnica@gmail.com>
2024-08-08 13:09:58 +0200

    .

--- Ordering.Application/CQRS/QueryHandlers/GetOrdersHandler - Copy.cs
+++ Ordering.Application/CQRS/QueryHandlers/GetOrdersHandler - Copy.cs
@@ -1,21 +1,17 @@
-﻿using AutoMapper;
-using BuildingBlocks.CQRS;
+﻿using BuildingBlocks.CQRS;
 using BuildingBlocks.Pagination;
 using Microsoft.Extensions.Logging;
 using Ordering.Application.CQRS.Queries;
 using Ordering.Application.Dtos;
-using Ordering.Domain.AggregatesModel.BuyerAggregate;
 
 namespace Ordering.Application.CQRS.QueryHandlers;
-public class GetOrdersHandler2(IBuyerRepository buyerRepository, IMapper mapper, ILogger<GetOrdersHandler> logger)
+public class GetOrdersHandler2(ILogger<GetOrdersHandler> logger)
     : IQueryHandler<DummyBuyersExample, DummyBuyersExampleResult>
 {
     public async Task<DummyBuyersExampleResult> Handle(DummyBuyersExample query, CancellationToken cancellationToken)
     {
         var pageIndex = query.PaginationRequest.PageIndex;
         var pageSize = query.PaginationRequest.PageSize;
-
-        var buyers = await buyerRepository.FindAsync("string");
 
         return new DummyBuyersExampleResult(
             new PaginatedResult<OrderDto>(
