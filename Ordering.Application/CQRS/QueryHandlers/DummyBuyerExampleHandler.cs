10cf47ef7b774e33ea84ffd5c4c9a9dafc59851f
Mateusz <mateusz.wolnica@gmail.com>
2024-08-08 13:19:11 +0200

    .

--- Ordering.Application/CQRS/QueryHandlers/DummyBuyerExampleHandler.cs
+++ Ordering.Application/CQRS/QueryHandlers/DummyBuyerExampleHandler.cs
@@ -1,17 +1,21 @@
 ﻿using BuildingBlocks.CQRS;
 using BuildingBlocks.Pagination;
+using MediatR;
 using Microsoft.Extensions.Logging;
 using Ordering.Application.CQRS.Queries;
 using Ordering.Application.Dtos;
+using Ordering.Domain.AggregatesModel.BuyerAggregate;
 
 namespace Ordering.Application.CQRS.QueryHandlers;
-public class DummyBuyerExampleHandler(ILogger<DummyBuyerExampleHandler> logger)
+public class DummyBuyerExampleHandler(IBuyerRepository repo, ILogger<DummyBuyerExampleHandler> logger)
     : IQueryHandler<DummyBuyersExample, DummyBuyersExampleResult>
 {
     public async Task<DummyBuyersExampleResult> Handle(DummyBuyersExample query, CancellationToken cancellationToken)
     {
         var pageIndex = query.PaginationRequest.PageIndex;
         var pageSize = query.PaginationRequest.PageSize;
+
+        var result = await repo.FindAsync("asdadsads");
 
         return new DummyBuyersExampleResult(
             new PaginatedResult<OrderDto>(
